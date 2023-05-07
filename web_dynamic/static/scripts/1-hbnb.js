847c6e0d2d2bf477767e9cd1150fb6ebd6035d85
Awe Ayomidipupo <pupoawe@gmail.com>
2023-05-07 20:02:17 +0100

    fix: get task 2 & 3 running

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,18 @@
-$('document').ready(function () {
+$("document").ready(function () {
   const amenities = {};
   $('INPUT[type="checkbox"]').change(function () {
-    if ($(this).is(':checked')) {
-      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    if ($(this).is(":checked")) {
+      amenities[$(this).attr("data-id")] = $(this).attr("data-name");
     } else {
-      delete amenities[$(this.attr('data-id')];
-
+      delete amenities[$(this).attr("data-id")];
     }
-    $('.amenities H4').text(Object.values(amenities).join(', '));
+    const amenitiesList = Object.values(amenities).join(", ");
+    $(".amenities H4").text(shortenString(amenitiesList));
   });
 });
+
+const shortenString = (string, length = 30) => {
+  if (string.length <= length) return string;
+
+  return string.substring(0, length - 3) + "...";
+};
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,24 +1,31 @@
-$('document').ready(function () {
+$("document").ready(function () {
   const amenities = {};
   $('INPUT[type="checkbox"]').change(function () {
-    if ($(this).is(':checked')) {
-      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    if ($(this).is(":checked")) {
+      amenities[$(this).attr("data-id")] = $(this).attr("data-name");
     } else {
-      delete amenities[$(this.attr('data-id')];
+      delete amenities[$(this).attr("data-id")];
     }
-    $('.amenities H4').text(Object.values(amenities).join(', '));
+    const amenitiesList = Object.values(amenities).join(", ");
+    $(".amenities H4").text(shortenString(amenitiesList));
   });
 
+  const shortenString = (string, length = 30) => {
+    if (string.length <= length) return string;
+
+    return string.substring(0, length - 3) + "...";
+  };
+
   $.ajax({
-    url: 'http://0.0.0.0:5001/api/v1/status/',
-    type: 'GET'
+    url: "http://0.0.0.0:5001/api/v1/status/",
+    type: "GET",
   }).done(function (response) {
-    if (response.status === 'OK') {
-      $('#api_status').addClass('available');
-      console.log(response.status)
+    if (response.status === "OK") {
+      $("#api_status").addClass("available");
+      console.log(response.status);
     } else {
-      $('#api_status').removeClass('available');
-      console.log(response.status)
+      $("#api_status").removeClass("available");
+      console.log(response.status);
     }
-   });
- });
+  });
+});
